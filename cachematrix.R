dcaa7498d596d25ed1a60e3f008d09bfa585ea24
ccenten <cjcenten@gmail.com>
2014-07-20 15:28:37 -0400

    Solution to Assignment2
    Solution to Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special matrix that can cache its inverse
+## cacheSolve computes the inverse of the special matrix returned by makeCacheMatrix.
+## If the inverse has already been calculated and the matrix has not changed, cache Solve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
+z<-matrix(rnorm(16),nrow=4,ncol=4)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+          x <<- y
+          m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      print(list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse))
 }
 
-
-## Write a short comment describing this function
+makeCacheMatrix(z)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
+
+cacheSolve(z)
+
+# testing
+amatrix<-makeCacheMatrix(z,nrow=2,ncol=2))
+amatrix$get()
+amatrix$
